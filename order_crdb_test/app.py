bad6171e4489e669f2511bd6ab18f0eed8444c64
Madelon <m.a.s.stol@student.tudelft.nl>
2022-05-16 16:17:11 +0200

    Temporary commit - Working on configuring cockroach integration for the order microservice

---
+++ order_crdb_test/app.py
@@ -0,0 +1,105 @@
+import os
+import atexit
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.orm
+from cockroachdb.sqlalchemy import run_transaction
+
+
+gateway_url = os.environ['GATEWAY_URL']
+
+app = Flask("order-service")
+app.config.from_pyfile('hello.cfg')
+db = SQLAlchemy(app)
+sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column('todo_id', db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    text = db.Column(db.String)
+    done = db.Column(db.Boolean)
+    pub_date = db.Column(db.DateTime)
+
+    def __init__(self, title, text):
+        self.title = title
+        self.text = text
+        self.done = False
+        self.pub_date = datetime.utcnow()
+
+
+@app.route('/')
+def show_all():
+    def callback(session):
+        return render_template(
+            'show_all.html',
+            todos=session.query(Todo).order_by(Todo.pub_date.desc()).all())
+    return run_transaction(sessionmaker, callback)
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['title']:
+            flash('Title is required', 'error')
+        elif not request.form['text']:
+            flash('Text is required', 'error')
+        else:
+            def callback(session):
+                todo = Todo(request.form['title'], request.form['text'])
+                session.add(todo)
+            run_transaction(sessionmaker, callback)
+            flash(u'Todo item was successfully created')
+            return redirect(url_for('show_all'))
+    return render_template('new.html')
+
+
+@app.route('/update', methods=['POST'])
+def update_done():
+    def callback(session):
+        for todo in session.query(Todo).all():
+            todo.done = ('done.%d' % todo.id) in request.form
+    run_transaction(sessionmaker, callback)
+    flash('Updated status')
+    return redirect(url_for('show_all'))
+
+
+if __name__ == '__main__':
+    app.run()
+
+def close_db_connection():
+    db.close()
+
+atexit.register(close_db_connection)
+
+
+@app.post('/create/<user_id>')
+def create_order(user_id):
+    pass
+
+
+@app.delete('/remove/<order_id>')
+def remove_order(order_id):
+    pass
+
+
+@app.post('/addItem/<order_id>/<item_id>')
+def add_item(order_id, item_id):
+    pass
+
+
+@app.delete('/removeItem/<order_id>/<item_id>')
+def remove_item(order_id, item_id):
+    pass
+
+
+@app.get('/find/<order_id>')
+def find_order(order_id):
+    return {"this": "is a", "json": "example"}
+
+
+@app.post('/checkout/<order_id>')
+def checkout(order_id):
+    pass
