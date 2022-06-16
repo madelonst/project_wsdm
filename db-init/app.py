08c00b31afe3c234c636a4f4da858e360eac1795
AaronvDiepen <aaronvdiepen@gmail.com>
2022-06-16 16:08:48 +0200

    Add indexing to cockroach

--- db-init/app.py
+++ db-init/app.py
@@ -5,20 +5,30 @@ conn = psycopg2.connect(db_url)
 
 with conn.cursor() as cur:
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS stock (item_id INT PRIMARY KEY, unit_price NUMERIC, stock_qty INT)"
+        "CREATE TABLE IF NOT EXISTS stock (item_id INT PRIMARY KEY, unit_price NUMERIC, stock_qty INT);"
     )
     conn.commit()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS order_headers (order_id INT PRIMARY KEY, user_id INT, paid BOOLEAN)"
+        "CREATE TABLE IF NOT EXISTS order_headers (order_id INT PRIMARY KEY, user_id INT, paid BOOLEAN);"
     )
     conn.commit()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS order_items (order_id INT, item INT, unit_price NUMERIC)"
+        "CREATE TABLE IF NOT EXISTS order_items (order_id INT, item INT, unit_price NUMERIC);"
     )
     conn.commit()
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS accounts (user_id INT PRIMARY KEY, credit NUMERIC)"
+        "CREATE TABLE IF NOT EXISTS accounts (user_id INT PRIMARY KEY, credit NUMERIC);"
     )
     conn.commit()
+    cur.execute(
+        "CREATE INDEX ON order_items(order_id);"
+    )
+    conn.commit()
+    cur.execute(
+        "CREATE INDEX ON order_items(user_id);"
+    )
+    conn.commit()
+
+
 
 conn.close()
